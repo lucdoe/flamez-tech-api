f87c3df4de8bd74c5dd01c742361b5a9fb267600
JanLucca Doerrstein <doerrstein.lucca@yahoo.de>
2019-04-17 16:03:33 +0200

    database connection and querry

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pymysql
+pymysql.install_as_MySQLdb()
+import MySQLdb
 
 app = Flask(__name__)
 
@@ -9,11 +12,37 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/contacts')
+@app.route('/contacts', methods=['GET', 'POST'])
 def contacts():
-    return render_template('contacts.html')
+
+    db = MySQLdb.connect("127.0.0.1", "root", "password", "flamez")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM user")
+    contacts = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    return render_template('contacts.html', contacts=contacts)
 
 
-@app.route('/inventory')
+@app.route('/inventory', methods=['GET', 'POST'])
 def inventory():
-    return render_template('inventory.html')
+    db = MySQLdb.connect("127.0.0.1", "root", "password", "flamez")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM item")
+    items = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return render_template('inventory.html', items=items)
+
+
+@app.route('/create_new', methods=['GET', 'POST'])
+def create_new():
+    db = MySQLdb.connect("127.0.0.1", "root", "password", "flamez")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM user")
+    user = cursor.fetchall()
+    cursor.close()
+    db.close()
+
+    return render_template('create_new.html', user=user)
