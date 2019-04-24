a98940df1b6156d75119f4bc0063fa1a68a9d108
Lucca <42179648+lucdoe@users.noreply.github.com>
2019-04-24 14:26:07 +0200

    passt

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
 from waitress import serve
-import flaskapp.__init__
+import flaskapp.flaskapp.py
 
-serve(flaskapp.__init__.app, host='0.0.0.0', port=80)
+serve(flaskapp.flaskapp.py.app, host='0.0.0.0', port=80)
